d98849db2d6fe4275a5e44881f20d29dc5b15b4a
alex-baiet <alex.baiet3@gmail.com>
2021-10-27 10:52:20 +0200

    Ajout portofio

---
+++ public/index.php
@@ -0,0 +1,63 @@
+
+<!DOCTYPE html>
+<html lang="fr" class="no_scroll">
+  <head>
+    <title>Alex BAIET | Acceuil</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+    <link rel="stylesheet" type="text/css" href="css/color.css">
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+  </head>
+
+  <body>
+    <section>
+      <div class="intro">
+        <h1>Bonjour.<br> Je suis<br> <b>Alex Baiet</b>,<br> Etudiant suivant le DUT informatique de Marne-la-Vallée.</h1>
+        <hr/>
+      </div>
+
+      <div class="menu">
+        <!--Page Portfolio-->
+        <a href="portfolio.php" class="choice">
+          <div class="background"></div>
+          <div class="content">
+            Portofolio
+          </div>
+        </a>
+        <hr/>
+
+        <!--Page Project-->
+        <a href="project.php" class="choice">
+          <div class="background"></div>
+          <div class="content">
+            Créations
+          </div>
+        </a>
+        <hr/>
+
+        <!--CV-->
+        <a href="cv.pdf" class="choice">
+          <div class="background"></div>
+          <div class="content">
+            CV
+          </div>
+        </a>
+        <hr/>
+
+        <!--Contact-->
+        <div class="choice contact">
+          <div class="background"></div>
+          <div class="content">
+            Contact
+          </div>
+          <div class="hidden_content">
+            <a href="mailto:alex.baiet3@gmail.com">alex.baiet3@gmail.com</a><br>
+            07 67 46 97 24
+          </div>
+        </div>
+      </div>
+    </section>
+    <script src="js/deco.js"></script>
+
+  </body>
+</html>
