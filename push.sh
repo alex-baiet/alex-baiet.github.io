65bfd27b2f50f59892f4dce89cba648d9d2c5233
alex-baiet <alex.baiet3@gmail.com>
2021-10-27 10:24:32 +0200

    osfd

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-git push "https://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@github.com/alex-baiet/$1"
+git push "https://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@github.com/alex-baiet/$1";
 
